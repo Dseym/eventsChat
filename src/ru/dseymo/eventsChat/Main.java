4bfff55d586102786931968651b69ff26e126b06
Dseym <kkovylin7@gmail.com>
2020-12-29 19:46:26 +0500

    Disable debug

--- src/ru/dseymo/eventsChat/Main.java
+++ src/ru/dseymo/eventsChat/Main.java
@@ -20,7 +20,7 @@ public class Main extends JavaPlugin {
 	
 	private EventsExecutor eventsExecutor;
 	@Getter
-	private boolean debug = true;
+	private boolean debug = false;
 	
 	public void onEnable() {
 		
